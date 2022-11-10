ca62bf1d60108e62cadab9e663d207631b8ab5fc
Lalith Kota <kotalalith@gmail.com>
2022-11-10 18:29:25 +0530

    Changed helm chart; Istio components and changed apt dependencies

--- packaging/docker-build.sh
+++ packaging/docker-build.sh
@@ -5,6 +5,8 @@
 ODOO_BASE_DIR="/opt/bitnami/odoo"
 EXTRA_ADDONS_DIR="$ODOO_BASE_DIR/extraaddons"
 
+apt-get update
+apt-get install -y build-essential autoconf libtool
 source $ODOO_BASE_DIR/venv/bin/activate
 for dir in $EXTRA_ADDONS_DIR/*/; do
     if [[ -f ${dir}requirements.txt ]]; then
@@ -12,3 +14,7 @@ for dir in $EXTRA_ADDONS_DIR/*/; do
     fi
 done
 deactivate
+apt-get purge -y build-essential autoconf libtool
+apt-get autoremove -y
+apt-get clean
+rm -rf /var/lib/apt/lists /var/cache/apt/archives
